1ac7fdc1d87551d83526b6baca0fa50c616d0cd2
Ruman-Islam <rumanislam48@gmail.com>
2022-06-10 18:35:28 +0600

    project added

--- src/pages/ProjectDetail/ProjectDetail.js
+++ src/pages/ProjectDetail/ProjectDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { SelectOutlined, DoubleRightOutlined, DoubleLeftOutlined } from '@ant-design/icons';
+import { SelectOutlined, DoubleRightOutlined, DoubleLeftOutlined, GithubOutlined } from '@ant-design/icons';
 import { Carousel } from 'antd';
 const contentStyle = {
     background: '#F2F3F3',
@@ -20,18 +20,39 @@ const ProjectDetail = () => {
     }, [id])
 
 
-
     return (
         <div className='w-8/12 mx-auto'>
-            <div className='mt-10'>
-                <div>
-                    <h1 className='text-[#CACACA] text-5xl font-bold'>{project?.name}</h1>
-                    <h1 className='text-[#8A8A8A] text-xl font-bold mt-2 mb-2'>{project?.title}</h1>
-                    <button className='text-white px-5 py-2 text-md font-semibold
-                     bg-[#00877A] flex justify-center items-center rounded-sm'>
-                        <SelectOutlined />
-                        <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/ruman-islam-6a567016a/' className='ml-2 uppercase'> Visit the website</a>
-                    </button>
+            <div className='py-10'>
+                <div className='flex justify-between'>
+                    <div>
+                        <h1 className='text-[#CACACA] text-5xl font-bold'>{project?.name}</h1>
+                        <h1 className='text-[#8A8A8A] text-xl font-bold mt-2 mb-2'>{project?.title}</h1>
+                    </div>
+                    <div className='flex flex-col'>
+                        <a target="_blank" rel="noopener noreferrer" href={project?.liveLink}
+                            className='ml-2 uppercase text-white px-5 py-2 text-md font-semibold
+                         bg-[#00877A] flex justify-center items-center rounded-sm
+                          hover:text-white hover:scale-105'>
+                            <SelectOutlined />
+                            <span className='ml-1'>Visit the website</span>
+                        </a>
+                        <div className='flex justify-between mt-3'>
+                            <a target="_blank" rel="noopener noreferrer" href={project?.gitClient}
+                                className='ml-2 uppercase text-white px-5 py-2 text-md font-semibold
+                         bg-[#00877A] flex justify-center items-center rounded-sm
+                          hover:text-white hover:scale-105'>
+                                <GithubOutlined />
+                                <span className='ml-1'>Client</span>
+                            </a>
+                            <a target="_blank" rel="noopener noreferrer" href={project?.gitServer}
+                                className='ml-2 uppercase text-white px-5 py-2 text-md font-semibold
+                         bg-[#00877A] flex justify-center items-center rounded-sm
+                          hover:text-white hover:scale-105 btn'>
+                                <GithubOutlined />
+                                <span className='ml-1'>Server</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div className='my-10 border border-slate-500'>
                     <Carousel swipeToSlide draggable arrows
@@ -46,8 +67,32 @@ const ProjectDetail = () => {
                     </Carousel>
                 </div>
                 <div>
-                    <h1 className='text-[#CACACA] text-4xl font-semibold'>Overview</h1>
-                    <p></p>
+                    <h1 className='text-[#CACACA] text-4xl font-semibold border-[#CACACA]
+                     border-t-0 border-l-0 border-r-0 border-b leading-loose'>Overview</h1>
+                    <p className='text-[#8A8A8A] text-xl mt-5 text-justify'>{project?.overview}</p>
+                </div>
+                <div className='my-5'>
+                    <h1 className='text-[#CACACA] text-4xl font-semibold border-[#CACACA]
+                     border-t-0 border-l-0 border-r-0 border-b leading-loose'>Features</h1>
+                    <ul className='list-disc pl-5'>
+                        {project?.features?.map((ft, index) =>
+                            <li key={index}
+                                className='text-[#8A8A8A] text-xl leading-loose'>
+                                {ft}
+                            </li>)}
+                    </ul>
+                </div>
+                <div>
+                    <h1 className='text-[#CACACA] text-4xl font-semibold border-[#CACACA]
+                     border-t-0 border-l-0 border-r-0 border-b leading-loose'>Technology Used</h1>
+                    <ul className='my-5 list-disc pl-5'>
+                        {project?.technology?.map((tech, index) =>
+                            <li
+                                className='text-xl text-[#8A8A8A] ml-2'
+                                key={index}>
+                                {tech},
+                            </li>)}
+                    </ul>
                 </div>
             </div>
         </div>
